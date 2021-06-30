d79a7ae5ff15d64236a621ec8d7e664e478e1417
KiranmayeeKK <katyayani.kolluru@gmail.com>
2021-06-30 20:22:40 +0200

    updated app.js with cors information

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ var checkCredentialAPIRouter = require('./routes/checkCredentialAPI')
 const fileupload = require("express-fileupload");
 var app = express();
 var cors = require('cors');
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
