27e64ee6331180775168f6e80512d82700aba7eb
KiranmayeeKK <katyayani.kolluru@gmail.com>
2021-06-30 20:42:06 +0200

    updated app.js with cors *

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors({
-  origin: 'https://ecstore-frontend.herokuapp.com'
+  origin: '*'
 }));
 app.use(logger('dev'));
 app.use(express.json());
